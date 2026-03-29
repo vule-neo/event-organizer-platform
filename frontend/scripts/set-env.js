9c060a4475ad51320b51df642a8055c967134753
PC <konevoliskolujos@gmail.com>
2026-03-29 15:03:43 +0200

    fix: support _DEV suffixed env vars in set-env script

--- frontend/scripts/set-env.js
+++ frontend/scripts/set-env.js
@@ -5,9 +5,9 @@ const targetPath = path.join(__dirname, '../src/environments/environment.prod.ts
 
 const envConfigFile = `export const environment = {
   production: true,
-  googleMapsApiKey: '${process.env.GOOGLE_MAPS_API_KEY || ''}',
-  apiUrl: '${process.env.API_URL || ''}',
-  apiBase: '${process.env.API_BASE || ''}'
+  googleMapsApiKey: '${process.env.GOOGLE_MAPS_API_KEY || process.env.GOOGLE_MAPS_API_KEY_DEV || ''}',
+  apiUrl: '${process.env.API_URL || process.env.API_URL_DEV || ''}',
+  apiBase: '${process.env.API_BASE || process.env.API_BASE_DEV || ''}'
 };
 `;
 
